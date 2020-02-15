5f30243571cc5df3bd459f00290e1f18a47d20ab
MyungHoon Jin <jinmang2@gmail.com>
2020-02-15 11:54:42 +0900

    Create 2-5.c

---
+++ CH02_상수와_데이터_출력/2-5.c
@@ -0,0 +1,15 @@
+/*
+	Author  : MyungHoon Jin
+	Chapter : 2장
+	Title   : 세 가지 진법의 정수 상수
+*/
+
+#include <stdio.h>  // stdio : standard input output(표준입출력)의 줄임말
+					// stdio.h의 내용을 프로그램 안에 복사
+
+int main(void) {
+	printf("%d\n", 12);     // 10진수 상수 출력
+	printf("%d\n", 014);    // 8진수 상수 출력
+    printf("%d\n", 0xc);    // 16진수 상수 출력
+	return 0;
+}
