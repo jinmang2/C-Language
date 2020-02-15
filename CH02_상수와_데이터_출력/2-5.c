6fbabf327d52bdb587d2febfe6aa2b72329d6ffb
MyungHoon Jin <jinmang2@gmail.com>
2020-02-15 12:00:45 +0900

    Update 2-5.c

--- CH02_상수와_데이터_출력/2-5.c
+++ CH02_상수와_데이터_출력/2-5.c
@@ -1,5 +1,5 @@
 /*
-    Author  : MyungHoon Jin
+	Author  : MyungHoon Jin
 	Chapter : 2장
 	Title   : 세 가지 진법의 정수 상수
 */
@@ -10,6 +10,10 @@
 int main(void) {
 	printf("%d\n", 12);     // 10진수 상수 출력
 	printf("%d\n", 014);    // 8진수 상수 출력
-    printf("%d\n", 0xc);    // 16진수 상수 출력
+	printf("%d\n", 0xc);    // 16진수 상수 출력
+
+	printf("%o\n", 12);		// 상수를 8진수로 출력
+	printf("%x\n", 12);		// 상수를 16진수 소문자로 출력
+	printf("%X\n", 12);		// 상수를 16진수 대문자로 출력
 	return 0;
 }
