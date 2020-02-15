f054aff1523c899162fcafb057bd365cfaae99e8
MyungHoon Jin <jinmang2@gmail.com>
2020-02-15 11:51:44 +0900

    Update 2-2.c

--- CH02_상수와_데이터_출력/2-2.c
+++ CH02_상수와_데이터_출력/2-2.c
@@ -1,10 +1,11 @@
 /*
-	Author : MyungHoon Jin
-	Title  : 문자열을 화면에 출력하는 프로그램
+	Author  : MyungHoon Jin
+	Chapter : 2
+	Title   : 문자열을 화면에 출력하는 프로그램
 */
 
-#include <stdio.h> // stdio : standard input output(표준입출력)의 줄임말
-				   // stdio.h의 내용을 프로그램 안에 복사
+#include <stdio.h>  // stdio : standard input output(표준입출력)의 줄임말
+					// stdio.h의 내용을 프로그램 안에 복사
 
 int main(void) {
 	printf("Be happy"); // printf: stdio.h안의 내장 입출력 함수
